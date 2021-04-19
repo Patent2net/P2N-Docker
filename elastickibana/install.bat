cc0050e894af3c92804634c712c33bb83a8c0d0e
Patent2net <patent2net@gmail.com>
2021-04-19 10:07:44 +0200

    Update install.bat
    Maj elastic version

--- elastickibana/install.bat
+++ elastickibana/install.bat
@@ -1,6 +1,6 @@
 REM this will install ES & kibana
-docker pull docker.elastic.co/elasticsearch/elasticsearch:7.9.2
-docker pull docker.elastic.co/kibana/kibana:7.9.2
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.12.0
+docker pull docker.elastic.co/kibana/kibana:7.12.0
 docker volume create volumeInd
 docker build --no-cache --tag elasticp2n .
 
