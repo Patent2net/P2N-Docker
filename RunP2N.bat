11fec1e6a14fd94a021583bcfdd70545a97d4087
Patent2net <patent2net@gmail.com>
2021-04-20 07:47:23 +0200

    optimisation

--- RunP2N.bat
+++ RunP2N.bat
@@ -1,4 +1,3 @@
-wsl -d docker-desktop  sysctl -w vm.max_map_count=262144
 docker compose -f p2n.yml up
 
 
--- install.bat
+++ install.bat
@@ -1,4 +1,8 @@
-docker build --no-cache --tag p2n config/ 
-docker run -t -d --name p2n -p 5000-5010:5000-5010  p2n || docker start p2n
-docker cp p2n:/home/p2n/P2N-V3 .
+docker build --no-cache --tag p2n-setup config/ 
+docker run -t -d --name p2n-setup -p 5000-5010:5000-5010  p2n-setup || docker start p2n-setup
+docker cp p2n-setup:/home/p2n/P2N-V3 .
+wsl -d docker-desktop  sysctl -w vm.max_map_count=262144
+docker container stop p2n-setup
+docker container rm p2n-setup
+
 
