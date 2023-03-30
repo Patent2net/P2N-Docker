d743ce000e95752bb0ad9459e98109dcd8640b11
Patent2net <patent2net@gmail.com>
2023-03-30 17:43:01 +0200

    little patch

--- startP2N.bat
+++ startP2N.bat
@@ -1,7 +1,9 @@
+docker start p2n
 docker cp .\config\sav\cles-epo.txt p2n:/home/p2n/P2N-V3/ 
 docker cp .\config\sav\dex.js p2n:/home/p2n/P2N-V3/ 
 docker cp .\config\sav\dex.json p2n:/home/p2n/P2N-V3/ 
 docker cp .\config\sav\run-all.sh p2n:/home/p2n/P2N-V3/ 
+docker stop p2n
 docker compose up -d
 docker exec -it p2n sh -c "chmod +x /home/p2n/P2N-V3/run-all.sh && /home/p2n/P2N-V3/run-all.sh"
 docker-compose logs -f p2n
