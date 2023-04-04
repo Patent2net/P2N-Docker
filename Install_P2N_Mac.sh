32532eb8a9d9a458af67c07f7baf78c15f721e11
Patent2net <patent2net@gmail.com>
2023-04-04 08:58:24 +0200

    MacOs update

--- Install_P2N_Mac.sh
+++ Install_P2N_Mac.sh
@@ -1,5 +1,4 @@
-SET DOCKER_BUILDKIT=0
-docker build --tag p2n-setup config/ 
+docker build --no-cache --tag p2n-setup config/ 
 docker run -t -d --name p2n-setup -p 5000-5010:5000-5010  p2n-setup || docker start p2n-setup
 REM Saving stuff in P2N - DATA is protected in docker volume
 move P2N-V3\dex.json .
@@ -13,6 +12,7 @@ del /Q /F P2N-V3
 docker cp p2n-setup:/home/p2n/P2N-V3 .
 wsl -d docker-desktop  sysctl -w vm.max_map_count=262144
 docker container stop p2n-setup
+docker container rm p2n-setup
 mklink /D DATA-P2N P2N-V3\DATA\
 copy P2N-V3\Patent2Net\templates\carrot2Data.html P2N-V3\DATA
 REM restore stuff
---
+++ Start_P2N_Mac.sh
@@ -0,0 +1,3 @@
+docker compose up
+
+open http://127.0.0.1:5000/
