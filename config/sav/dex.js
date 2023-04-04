3a2831e94b776a64beeabe0b3fb772b33e669d83
Patent2net <patent2net@gmail.com>
2023-04-04 10:19:18 +0200

    update
    the use of a prebuild image

--- config/sav/dex.js
+++ config/sav/dex.js
@@ -1,8 +1,6 @@
 document.write('\
 <ul>\
-<li><a href="DATA/lentille2.html" target="_blank">lentille2</a></li>\
+<li><a href="DATA/banana.html" target="_blank">banana</a></li>\
 <li><a href="DATA/lentille.html" target="_blank">lentille</a></li>\
-<li><a href="DATA/leader.html" target="_blank">leader</a></li>\
-<li><a href="DATA/***dataDir***.html" target="_blank">***dataDir***</a></li>\
 </ul>\
 ');
